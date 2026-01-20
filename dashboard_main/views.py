f68e5009a911046b10b79af90c0a917913dd636e
Afridi <afridisultan.charbindu@gmail.com>
2026-01-20 16:29:21 +0600

    Implemented edit and delete functionality for category on dashboard

--- dashboard_main/views.py
+++ dashboard_main/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from blog.models import Category, Blog
-
+from dashboard_main.forms import CategoryForm
 @login_required(login_url='login_page')
 def dashboard(request):
     category_count = Category.objects.all().count()
@@ -23,3 +23,38 @@ def dashboard_blogs(request):
         'all_blogs':all_blogs
     }
     return render(request,'dashboard/blogs.html',context)
+
+#add category in dashboard
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard_category')
+    else:
+        form = CategoryForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'dashboard/add_category.html',context)
+
+#delete the category from the dashboard
+def delete_category(request,pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    return redirect('dashboard_category')
+
+#Edit category from dashboard
+def edit_category(request,pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard_category')
+    form = CategoryForm(instance=category)
+    context = {
+        'form':form,
+        'category':category
+    }
+    return render(request, 'dashboard/edit_category.html',context)
